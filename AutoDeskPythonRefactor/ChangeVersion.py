2274c02f0ed42ca7ef2ea9d99628aea786f8a29c
Rahul Chakraborty <Rahul.Chakraborty2@cbre.com>
2019-12-24 10:05:31 +0530

    Refactored

--- AutoDeskPythonRefactor/ChangeVersion.py
+++ AutoDeskPythonRefactor/ChangeVersion.py
@@ -8,16 +8,16 @@ region = "global"
 source_folder = "src"
 
 # environment variable name to store project root folder path
-source_path_env_variable_Name = "SourcePath"
+source_path_env_var = "SourcePath"
 
 # defining environment variable names for SConstruct File's build parameters
-sconstruct_key_and_env_variable_point = "point"
-sconstruct_key_and_env_variable_patch = "patch"
-sconstruct_key_and_env_variable_major = "major"
-sconstruct_key_and_env_variable_minor = "minor"
+sconstruct_env_var_param_point = "point"
+sconstruct_env_var_param_patch = "patch"
+sconstruct_env_var_param_major = "major"
+sconstruct_env_var_param_minor = "minor"
 
 # defining environment variable names for VERSION File's build parameters
-version_key_and_env_variable_point = "ADLMSDK_VERSION_POINT"
+version_env_var_param_point = "ADLMSDK_VERSION_POINT"
 
 
 # #SCONSTRUCT file interesting line
@@ -30,14 +30,14 @@ version_key_and_env_variable_point = "ADLMSDK_VERSION_POINT"
 def update_s_construct(file_name):
     ''' Function to update build number in SConstruct File '''
     ret_val = 0
-    if update_version_number_in_file(file_name, sconstruct_key_and_env_variable_point,
-                                     sconstruct_key_and_env_variable_point) != 0: ret_val = 1
-    if update_version_number_in_file(file_name, sconstruct_key_and_env_variable_major,
-                                     sconstruct_key_and_env_variable_major) != 0: ret_val = 1
-    if update_version_number_in_file(file_name, sconstruct_key_and_env_variable_minor,
-                                     sconstruct_key_and_env_variable_minor) != 0: ret_val = 1
-    if update_version_number_in_file(file_name, sconstruct_key_and_env_variable_patch,
-                                     sconstruct_key_and_env_variable_patch) != 0: ret_val = 1
+    if update_build_number_in_file(file_name, sconstruct_env_var_param_point,
+                                   sconstruct_env_var_param_point) != 0: ret_val = 1
+    if update_build_number_in_file(file_name, sconstruct_env_var_param_major,
+                                   sconstruct_env_var_param_major) != 0: ret_val = 1
+    if update_build_number_in_file(file_name, sconstruct_env_var_param_minor,
+                                   sconstruct_env_var_param_minor) != 0: ret_val = 1
+    if update_build_number_in_file(file_name, sconstruct_env_var_param_patch,
+                                   sconstruct_env_var_param_patch) != 0: ret_val = 1
     return ret_val
 
 
@@ -45,18 +45,18 @@ def update_s_construct(file_name):
 # ADLMSDK_VERSION_POINT=6
 def update_version(file_name):
     ''' Function to update build number in VERSION file'''
-    return update_version_number_in_file(file_name, version_key_and_env_variable_point,
-                                         version_key_and_env_variable_point)
+    return update_build_number_in_file(file_name, version_env_var_param_point,
+                                       version_env_var_param_point)
 
 
-def update_version_number_in_file(file, build_parameter, version_info_env_key):
+def update_build_number_in_file(file, build_parameter, build_number_key):
     """
     Function to update the build number in any given file w.r.t a specified build parameter.
     User can either provide the build information through specific environment variable for each
     Build parameter or can provide the same in config.properties file.
     :param file: The file path for in which Build number has to be updated
     :param build_parameter: The build parameter name as string for the specified file
-    :param version_info_env_key: The environment variable or config file key which stores the build number
+    :param build_number_key: The environment variable or config file key which stores the build number
     :return: Returns 0 if build information is updated; 1 for all other conditions
     """
     version_number = 0
@@ -67,22 +67,22 @@ def update_version_number_in_file(file, build_parameter, version_info_env_key):
     if get_config_data("General", "read_version_from_config") == 'N':
 
         # validates if the corresponding environment variables are defined and not empty
-        if version_info_env_key in os.environ and os.getenv(version_info_env_key) is not None:
+        if build_number_key in os.environ and os.getenv(build_number_key) is not None:
 
             # validates if the source path is defined in env. variable, else considers current working directory
-            if source_path_env_variable_Name in os.environ and os.getenv(source_path_env_variable_Name) is not None:
-                source_path = os.getenv(source_path_env_variable_Name)
+            if source_path_env_var in os.environ and os.getenv(source_path_env_var) is not None:
+                source_path = os.getenv(source_path_env_var)
             else:
                 source_path = os.getcwd()
 
             # reads build number from env. variable
-            version_number = os.getenv(version_info_env_key.upper())
+            version_number = os.getenv(build_number_key.upper())
             flag = 1
         else:
-            print("no environment variable defined: " + version_info_env_key + " or " + source_path_env_variable_Name)
+            print("no environment variable defined: " + build_number_key + " or " + source_path_env_var)
     else:
         # reads build numbers from config file & source path as current working directory
-        version_number = get_config_data("AllVersion", version_info_env_key.lower())
+        version_number = get_config_data("AllVersion", build_number_key.lower())
         source_path = os.getcwd()
         flag = 1
 
@@ -115,8 +115,8 @@ def update_version_number_in_file(file, build_parameter, version_info_env_key):
             os.rename(dest_file, source_file)
 
             # console log statement
-            print("updated version '"+build_parameter+"' in file '"+source_file+"' to value :" +
-                  os.getenv(version_info_env_key.upper()))
+            print("updated version '" + build_parameter +"' in file '" + source_file +"' to value :" +
+                  os.getenv(build_number_key.upper()))
 
             return 0
         else:
@@ -135,7 +135,7 @@ def test_no_environment_variable(file_name):
     ''' Function to test when there is no environment variable defined for build number '''
 
     if get_config_data("General", "read_version_from_config") == 'N':
-        return update_version_number_in_file(file_name, "ADLMSDK_VERSION_POINT", "InvalidVariable")
+        return update_build_number_in_file(file_name, "ADLMSDK_VERSION_POINT", "InvalidVariable")
     else:
         return 1
 
@@ -152,7 +152,15 @@ def get_config_data(section_name, key):
     config.read(os.path.join(os.getcwd(), "config.properties"))
 
     details_dict = dict(config.items(section_name))
-    return details_dict[key]
+    if check_key_in_dict(details_dict, key) == 0: return details_dict[key]
+    else: return 1
+
+
+def check_key_in_dict(dict, key):
+    if key in dict:
+        return 0
+    else:
+        return 1
 
 
 main()
